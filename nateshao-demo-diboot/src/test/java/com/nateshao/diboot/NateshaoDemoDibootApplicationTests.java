53dfedd5622f180bc2677e1a8009e869dd5156f4
nateshao <shaotongjie@gmail.com>
2020-12-23 11:24:43 +0800

    nateshao-boot-diboot

---
+++ nateshao-demo-diboot/src/test/java/com/nateshao/diboot/NateshaoDemoDibootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nateshao.diboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NateshaoDemoDibootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
