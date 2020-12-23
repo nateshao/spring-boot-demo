407cb99d6b22b33236d01271162e59a019bb491e
nateshao <shaotongjie@gmail.com>
2020-12-23 19:16:04 +0800

    代码优化

--- nateshao-demo-war/src/main/java/com/nateshao/war/NateshaoDemoWarApplication.java
+++ nateshao-demo-war/src/main/java/com/nateshao/war/NateshaoDemoWarApplication.java
@@ -2,12 +2,20 @@ package com.nateshao.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NateshaoDemoWarApplication {
+public class NateshaoDemoWarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NateshaoDemoWarApplication.class, args);
     }
-
+    /**
+     * 若需要打成 war 包，则需要写一个类继承 {@link SpringBootServletInitializer} 并重写 {@link SpringBootServletInitializer#configure(SpringApplicationBuilder)}
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NateshaoDemoWarApplication.class);
+    }
 }
